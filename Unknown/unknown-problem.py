33498363a309cdcb72f4d62e1c1c0536d7de2f36
DevaNandanJS <devanandanjs2004@gmail.com>
2025-06-20 00:42:05 +0530

    Add solution: Mini-Max Sum (Unknown)

---
+++ Unknown/unknown-problem.py
@@ -0,0 +1,33 @@
+/*
+Problem: Mini-Max Sum
+Difficulty: Unknown
+URL: https://www.hackerrank.com/dashboard
+Language: python
+Date: 2025-06-19
+*/
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    arr.sort(reverse=True)
+    minn = sum(arr[1::])
+    maxx = sum(arr[:len(arr)-1:])
+    print(minn,maxx)
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
