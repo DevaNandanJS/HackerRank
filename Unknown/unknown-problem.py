b53c6b815587e52f15a8257945b11c219fe97012
DevaNandanJS <devanandanjs2004@gmail.com>
2025-06-20 01:30:07 +0530

    Update solution: unknown-problem (Unknown)

--- Unknown/unknown-problem.py
+++ Unknown/unknown-problem.py
@@ -27,7 +27,7 @@ def findMedian(arr):
     a = arr.sort()
     n = len(a)
     median = n//2
-    return mew
+    return median
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
