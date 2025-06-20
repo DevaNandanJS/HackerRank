afa00ea98e0c5374e541d5534e39f2b5854b31f0
DevaNandanJS <devanandanjs2004@gmail.com>
2025-06-21 02:24:42 +0530

    Add solution: Lonely Integer (Unknown)

---
+++ Unknown/one-week-preparation-kit-lonely-integer.py
@@ -0,0 +1,47 @@
+/*
+Problem: Lonely Integer
+Difficulty: Unknown
+URL: https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-two
+Language: python
+Date: 2025-06-20
+*/
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    
+    m=0
+    for i in range (len(a)):
+        ab=0
+        for j in range (len(a)):
+            if a[i] == a[j]:
+                ab = ab+1
+        if ab == 1:
+            m = a[i]
+    return m
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
